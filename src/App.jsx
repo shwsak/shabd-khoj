d0981305ca1d1af1d705030c91aa74dc808443fa
Shweta <shwetaa9497@gmail.com>
2022-12-25 18:34:51 +0530

    refactored searchcard component

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
   async function getDictionary() {
 
     let word = inputRef.current.value;
-    
+
     setIsLoading(true);
 
     if (!word) {
@@ -44,43 +44,9 @@ function App() {
     setIsLoading(false);
 
   }
- 
 
-  const dictData = dictionary[0];   
-  let phonetic = dictData.phonetic || "";
+  const dictionaryData = dictionary[0];
 
-
-  let meanings;
-
-  if (dictData.meanings) {
-    meanings = dictData.meanings.map((m) => {
-      const pos = m.partOfSpeech;
-      const definitions = m.definitions.map((d) => {
-        const definition = d.definition;
-        return <li>{definition}</li>
-      })
-      return <div className='text-dark'>
-        <h4 className='fst-italic card-pos'>{pos}</h4>
-        <ol key={m.definitions}>{definitions}</ol>
-      </div>
-
-    })
-  }
-
-  let phonecticsArray = dictData.phonetics || "";
-  let getAudio = phonecticsArray[0];
-
-  let audio = getAudio ? getAudio.audio : "";
-
-  function handleSound() {
-    let playAudio = new Audio(audio);
-    playAudio.play()
-
-    if(playAudio === "undefined"){
-      alert("sorry no audio is found")
-    }
-    
-  }
 
   return (
     <>
@@ -89,11 +55,8 @@ function App() {
         <div className='loader'>
           <MoonLoader color="#f79256" size={38} />
         </div>
-        : (meanings ?
-          <SearchCard word={searchWord} phonectics={phonetic} meanings={meanings} audio={handleSound} />
-          : <div className="alert alert-danger mt-5 mx-4" role="alert">
-            "{searchWord}" meaning not found
-          </div>)
+        :
+        <SearchCard data={dictionaryData} word={searchWord} />
       }
     </>
 
--- src/components/SearchCard.jsx
+++ src/components/SearchCard.jsx
@@ -2,41 +2,73 @@
 
 function SearchCards(props) {
 
-    return (
-        <div className="container my-5">
-            <div className="card text-light m-auto">
-                <div className="card-body text-start p-4">
 
-                    <div className="lh-1">
-                        <div className="d-flex">
-                            <div className="mt-3" onClick={props.audio}>
-                                <span className="card-icon">
-                                    <i className="fas fa-volume-up "></i>
-                                </span>
-                            </div>
-                            <div className="container">
-                                <div className="row">
-                                    <div className="col">
-                                        <p className="card-word">{props.word}</p>
+    let phonecticsArray = props.data.phonetics || "";
+    let getAudio = phonecticsArray[0];
+    let audio = getAudio ? getAudio.audio : "";
 
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="col">
-                                        <p className="text-dark">{props.phonectics}</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <hr className="text-dark" />
-                    </div>
-                    
-                    {/* meanings */}
-                    {props.meanings}
+    function handleProunciation() {
+        let playAudio = new Audio(audio);
+        playAudio.play()
+    }
 
-                </div>
+    let phonetic = props.data.phonetic;
+
+    let meanings;
+    if (props.data.meanings) {
+        meanings = props.data.meanings.map((m) => {
+            const pos = m.partOfSpeech;
+            const definitions = m.definitions.map((d) => {
+                const definition = d.definition;
+                return <li>{definition}</li>
+            })
+            return <div className='text-dark'>
+                <h4 className='fst-italic card-pos'>{pos}</h4>
+                <ol key={m.definitions}>{definitions}</ol>
             </div>
-        </div>
+
+        })
+    }
+
+    return (
+
+        <>
+            {meanings ?
+                <div className="container my-5">
+                    <div className="card text-light m-auto">
+                        <div className="card-body text-start p-4">
+                            <div className="lh-1">
+                                <div className="d-flex">
+                                    <div className="mt-3" onClick={handleProunciation}>
+                                        <span className="card-icon">
+                                            <i className="fas fa-volume-up "></i>
+                                        </span>
+                                    </div>
+                                    <div className="container">
+                                        <div className="row">
+                                            <div className="col">
+                                                <p className="card-word">{props.word}</p>
+                                            </div>
+                                        </div>
+                                        <div className="row">
+                                            <div className="col">
+                                                <p className="text-dark">{phonetic}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <hr className="text-dark" />
+                            </div>
+                            {meanings}
+                        </div>
+                    </div>
+                </div>
+                :
+                <div className="alert alert-danger mt-5 mx-4" role="alert">
+                    {props.word} meaning not found
+                </div>
+            }
+        </>
 
     );
 }
